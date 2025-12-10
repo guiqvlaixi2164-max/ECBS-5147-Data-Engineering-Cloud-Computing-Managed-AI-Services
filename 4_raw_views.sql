67902f694d132b52e6cd2e1bf2b9c0cfc82f732f
Jiaqi Pan <guiqvlaixi2164@gmail.com>
2025-12-10 18:29:07 +0100

    Delete 4_raw_views.sql

--- 4_raw_views.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE
-jiaqi.raw_views (
-    title STRING,
-    views INT,
-    rank INT,
-    date DATE,
-    retrieved_at STRING)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-LOCATION 's3://jiaqi-wikidata/raw-views/';
