97c98c44fcfa051abb751df6ad1d1b08f14f0fa3
Jiaqi Pan <guiqvlaixi2164@gmail.com>
2025-12-10 18:29:18 +0100

    Delete 5_views_view.sql

--- 5_views_view.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW jiaqi.views AS
-    SELECT
-        title,
-        views,
-        rank,
-        date,
-        cast(from_iso8601_timestamp(retrieved_at) AS TIMESTAMP) as retrieved_at
-    FROM jiaqi.raw_views
